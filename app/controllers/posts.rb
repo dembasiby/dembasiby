00a291c705b56a91c861b4b20be37a12ed73c768
Demba Siby <dembasiby@gmail.com>
2017-08-03 09:48:36 +0000

    Created the routes for viewing a single post and all posts.

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,30 +1,13 @@
 Dembasiby::App.controllers :posts do
-  
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
 
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
   get :index do
-
+    @posts = Post.reverse_order(:created_at).all
+    render :erb, 'posts/index'
   end
 
-  get :show do
-
+  get :show, :with => :id do
+    @post = Post[id: params[:id]]
+    render :erb, 'posts/show'
   end
 
 end
