300c72707590afa80e1002285f24288122b9de96
Demba Siby <dembasiby@gmail.com>
2017-08-03 14:51:10 +0000

    Added the association to the account model.

--- models/post.rb
+++ models/post.rb
@@ -1,9 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :account
 
-  plugin :validation_helpers
   def validate
     super
-    validate_presence [:title, :body]
+    validate_presence_of [:title, :body]
   end
 end
